eb4e54e7026aad3a233b343498bffa1cab7d92a9
Daniel Parks <daniel.parks@bbc.co.uk>
2018-06-13 17:14:41 +0100

    Refactor

--- build.sbt
+++ build.sbt
@@ -33,7 +33,11 @@ SbtPgp.autoImport.useGpgAgent := true
 
 libraryDependencies ++= Seq(
   "com.twitter" %% "util-core" % "18.5.0",
+  "com.twitter" %% "finagle-thriftmux" % "18.5.0",
   "org.scala-sbt" % "test-interface" % "1.0",
+//  "ch.qos.logback" % "logback-classic" % "1.2.3",
+//  "com.typesafe.scala-logging" %% "scala-logging" % "3.9.0",
+  "bbc.rms" %% "sounds-metadata" % "0.1-SNAPSHOT",
   "org.specs2" %% "specs2-core" % "3.9.2" % "test"
 )
 
--- src/test/scala/com/delprks/massiveattack/method/ThriftLoadTestSpecTest.scala
+++ src/test/scala/com/delprks/massiveattack/method/ThriftLoadTestSpecTest.scala
@@ -1,23 +1,49 @@
 package com.delprks.massiveattack.method
 
+import bbc.rms.metadata.thriftscala.{PlayableItem, ProgrammesService}
+import bbc.rms.metadata.thriftscala.ProgrammesService.PlayableItems
+import com.twitter.finagle.{Thrift, client}
+import com.twitter.finagle.builder.ClientBuilder
+import com.twitter.finagle.thriftmux.service.ThriftMuxResponseClassifier
+import com.twitter.util
 import org.specs2.mutable.Specification
 
 import scala.concurrent.Future
 
 class ThriftLoadTestSpecTest extends Specification {
-  def longRunningMethod(): Future[Seq[String]] = {
-    Thread.sleep(20)
+  //  def longRunningMethod(): Future[Seq[String]] = {
+  //    Thread.sleep(20)
+  //
+  //    Future.successful(Seq("Method finished running"))
+  //  }
+  //
+  //  "PlayableItemMapper" should {
+  //    "load test first expensive method" in {
+  //      val loadTestSpec = new MethodLoadTest(threads = 600, invocations = 5000000, duration = 20)
+  //
+  //      loadTestSpec.test(() => longRunningMethod())
+  //
+  //      1 shouldEqual (1)
+  //    }
+  //  }
 
-    Future.successful(Seq("Method finished running"))
-  }
+  "Playable endpoint should perform well" in {
+    val host = "localhost:9911"
 
-  "PlayableItemMapper" should {
-    "load test first expensive method" in {
-      val loadTestSpec = new MethodLoadTest(threads = 600, invocations = 5000000, duration = 20)
+    val client: ProgrammesService.MethodPerEndpoint = Thrift.client.build[ProgrammesService.MethodPerEndpoint](host)
 
-      loadTestSpec.test(() => longRunningMethod())
+    val pids = Seq()
 
-      1 shouldEqual (1)
-    }
+    val testProperties = MethodTestProperties(
+      threads = 20,
+      invocations = 10000000,
+      duration = 310
+    )
+
+    val loadTestSpec = new MethodLoadTest(testProperties)
+
+    loadTestSpec.test(() => client.playableItems(pids))
+
+    1 shouldEqual 1
   }
 }
